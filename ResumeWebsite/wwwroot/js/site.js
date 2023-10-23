b95c73c3572e974a99bbae73bfac4520fca9675c
Sentrial <bboyblowup@gmail.com>
2023-10-22 22:25:21 -0600

    Changed text under name, added navbar items that now scroll you to different points on the page, navbar now dissapears when scrolling down and reappears when scrolling up, added dagger logo to top right.

--- ResumeWebsite/wwwroot/js/site.js
+++ ResumeWebsite/wwwroot/js/site.js
@@ -2,3 +2,26 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+let prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', function () {
+
+    console.log("called");
+
+    const currentScrollPos = window.pageYOffset;
+
+    if (window.pageYOffset == 0) {
+       document.getElementById("navbar").classList.remove("scroll-up")
+    }
+    else if (prevScrollPos > currentScrollPos) {
+        document.getElementById("navbar").classList.remove("scroll-down");
+        document.getElementById("navbar").classList.add("scroll-up");
+    }
+    else {
+        document.getElementById("navbar").classList.remove("scroll-up");
+        document.getElementById("navbar").classList.add("scroll-down");
+    }
+
+    prevScrollPos = currentScrollPos;
+})

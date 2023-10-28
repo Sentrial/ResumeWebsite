092784403c271ae416fe94521ca8f58940c25709
Sentrial <bboyblowup@gmail.com>
2023-10-27 22:20:36 -0600

    Button selection for projects now works. Code cleanup.

--- ResumeWebsite/wwwroot/js/site.js
+++ ResumeWebsite/wwwroot/js/site.js
@@ -8,8 +8,6 @@ let prevScrollPos = window.pageYOffset;
 
 window.addEventListener('scroll', function () {
 
-    console.log("called");
-
     const currentScrollPos = window.pageYOffset;
 
     if (window.pageYOffset == 0) {
@@ -33,87 +31,39 @@ let distressedButton = document.getElementById("distressed-button");
 let distressed2Button = document.getElementById("distressed2-button");
 let fafButton = document.getElementById("floor-after-floor-button");
 
-distressedButton.addEventListener("click", () => {
-    projectsContainer.classList.add("distressed-selected");
-    projectsContainer.classList.remove("distressed2-selected");
-    projectsContainer.classList.remove("floor-after-floor-selected");
+distressedButton.addEventListener("click", (e) => {
+    window.scrollTo(0, prevScrollPos);
+
+    projectsSliderContainer.classList.add("distressed-selected");
+    projectsSliderContainer.classList.remove("distressed2-selected");
+    projectsSliderContainer.classList.remove("floor-after-floor-selected");
+
+    distressedButton.classList.add("selected");
+    distressed2Button.classList.remove("selected");
+    fafButton.classList.remove("selected");
 })
 
-distressed2Button.addEventListener("click", () => {
-    projectsContainer.classList.add("distressed2-selected");
-    projectsContainer.classList.remove("distressed-selected");
-    projectsContainer.classList.remove("floor-after-floor-selected");
+distressed2Button.addEventListener("click", (e) => {
+    window.scrollTo(0, prevScrollPos);
+    
+    projectsSliderContainer.classList.add("distressed2-selected");
+    projectsSliderContainer.classList.remove("distressed-selected");
+    projectsSliderContainer.classList.remove("floor-after-floor-selected");
+
+    distressed2Button.classList.add("selected");
+    distressedButton.classList.remove("selected");
+    fafButton.classList.remove("selected");
+
 })
 
-fafButton.addEventListener("click", () => {
-    projectsContainer.classList.add("floor-after-floor-selected");
-    projectsContainer.classList.remove("distressed-selected");
-    projectsContainer.classList.remove("distressed2-selected");
+fafButton.addEventListener("click", (e) => {
+    window.scrollTo(0, prevScrollPos);
+
+    projectsSliderContainer.classList.add("floor-after-floor-selected");
+    projectsSliderContainer.classList.remove("distressed-selected");
+    projectsSliderContainer.classList.remove("distressed2-selected");
+
+    fafButton.classList.add("selected");
+    distressedButton.classList.remove("selected");
+    distressed2Button.classList.remove("selected");
 })
-
-
-
-/*document.getElementById("distressed-button").onclick = function () {
-    console.log("distressed button clicked");
-
-    document.getElementById("distressed-button").classList.add("selected");
-    document.getElementById("distressed2-button").classList.remove("selected");
-    document.getElementById("floor-after-floor-button").classList.remove("selected");
-
-    document.getElementById("distressed-content").classList.remove("hide-project");
-    document.getElementById("distressed-content").classList.remove("idle-project");
-    document.getElementById("distressed2-content").classList.add("hide-project");
-    document.getElementById("floor-after-floor-content").classList.add("hide-project");
-
-    document.getElementById("distressed-content").classList.add("show-project");
-    document.getElementById("distressed2-content").classList.remove("show-project");
-    document.getElementById("floor-after-floor-content").classList.remove("show-project");
-
-    setTimeout(setProjectContent, 500, "distressed2-content", "floor-after-floor-content");
-}
-
-document.getElementById("distressed2-button").onclick = function () {
-    console.log("distressed2 button clicked");
-
-    document.getElementById("distressed2-button").classList.add("selected");
-    document.getElementById("distressed-button").classList.remove("selected");
-    document.getElementById("floor-after-floor-button").classList.remove("selected");
-
-    document.getElementById("distressed2-content").classList.remove("hide-project");
-    document.getElementById("distressed2-content").classList.remove("idle-project");
-    document.getElementById("distressed-content").classList.add("hide-project");
-    document.getElementById("floor-after-floor-content").classList.add("hide-project");
-
-    document.getElementById("distressed2-content").classList.add("show-project");
-    document.getElementById("distressed-content").classList.remove("show-project");
-    document.getElementById("floor-after-floor-content").classList.remove("show-project");
-
-    setTimeout(setProjectContent, 500, "distressed-content", "floor-after-floor-content");
-}
-
-document.getElementById("floor-after-floor-button").onclick = function () {
-    console.log("Floor After Floor button clicked");
-
-    document.getElementById("floor-after-floor-button").classList.add("selected");
-    document.getElementById("distressed-button").classList.remove("selected");
-    document.getElementById("distressed2-button").classList.remove("selected");
-
-    document.getElementById("floor-after-floor-content").classList.remove("hide-project");
-    document.getElementById("floor-after-floor-content").classList.remove("idle-project");
-    document.getElementById("distressed-content").classList.add("hide-project");
-    document.getElementById("distressed2-content").classList.add("hide-project");
-
-    document.getElementById("floor-after-floor-content").classList.add("show-project");
-    document.getElementById("distressed-content").classList.remove("show-project");
-    document.getElementById("distressed2-content").classList.remove("show-project");
-
-    setTimeout(setProjectContent, 500, "distressed-content", "distressed2-content");
-}
-
-function setProjectContent(name1, name2) {
-    document.getElementById(name1).classList.remove("hide-project");
-    document.getElementById(name2).classList.remove("hide-project");
-
-    document.getElementById(name1).classList.add("idle-project");
-    document.getElementById(name2).classList.add("idle-project");
-}*/
